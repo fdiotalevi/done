420ea79711cb95788165b0d49c43881683eb4c8d
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2013-09-18 22:48:41 +0100

    created first migration file

--- docs/database/01_user.sql
+++ docs/database/01_user.sql
@@ -1,1 +1,9 @@
 -- create user table here
+CREATE TABLE `user` (
+  `email` varchar(256) NOT NULL,
+  `firstname` varchar(256) NOT NULL,
+  `lastname` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  PRIMARY KEY (`email`)
+);
+
