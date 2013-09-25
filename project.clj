5d3c93dbe131808e207482eaba3d606c0754a7df
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2013-09-25 20:00:37 +0100

    finally gave up on clojurescript, jayq and went back to jquery

--- project.clj
+++ project.clj
@@ -9,21 +9,13 @@
                  [mysql/mysql-connector-java "5.1.6"]
                  [com.jolbox/bonecp "0.8.0-rc3" :exclusions [org.slf4j/slf4j-api com.google.guava/guava]]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
-                 [org.slf4j/slf4j-log4j12 "1.7.5"]
-                 [org.clojure/clojurescript "0.0-1889" :exclusions [org.apache.ant/ant]]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]
                  [bouncer "0.3.0-alpha1"]
-                 [org.clojure/data.json "0.2.2"]
-                 [jayq "2.4.0"]]
-  :plugins [[lein-ring "0.8.5"]
-            [lein-cljsbuild "0.3.3"]]
-  :cljsbuild {
-    :builds [{:source-paths ["src-cljs"]
-              :compiler {:output-to "resources/public/js/main.js"
-                         :optimizations :whitespace}}]}
+                 [org.clojure/data.json "0.2.2"]]
+  :plugins [[lein-ring "0.8.5"]]
   :ring {:handler done.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
--- src-cljs/done/main.cljs
+++
@@ -1,9 +0,0 @@
-(ns done.hello
-  (:require [jayq.core :as jq]))
-
-(defn show-login [] (log "log in"))
-
-(jq/ajax "/api/sessions/me" {:success show-login :error show-login} )
-
-;(log (str "a"))
-
