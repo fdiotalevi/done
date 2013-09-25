51afecabf3431e366c0babec66b2728f07f73f22
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2013-09-25 21:47:27 +0100

    refactored and objectified the js

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,56 +1,71 @@
 (function($) {
-    
-    
+
     var _compileTemplate = function(templateId, jsonObj) {
         var template = $(templateId).html();
         return Mustache.compile(template,  ["[[", "]]"])(jsonObj);
     },
-    _showDones = function() {
-        $.ajax({
-            type: 'GET',
-            url: '/api/dones',
-            dataType: 'json',
-            success: function(data) { 
-                $('#content').html(_compileTemplate('#dones-template', data));
-            }
-        });
-    }, 
-    _showOrHideLoginForm = function() {
-        $.ajax("/api/sessions/me")
-            .done(function() { 
-                $('#login').hide(); 
-                $('#logout').show();
-                _showDones();
-            })
-            .fail(function() { 
-                $('#login').show(); 
-                $('#logout').hide();
+    Dones = function() {
+        var _this = this;
+
+        this.show = function() {
+            $.ajax({
+                type: 'GET',
+                url: '/api/dones',
+                dataType: 'json',
+                success: function(data) { 
+                    $('#content').html(_compileTemplate('#dones-template', data));
+                }
             });
+        };
+
+        this.delete = function() {
+            $('#content').html('');  
+        };
     },
-    _loginForm = function() {
-        $('#login-form').on('submit', function() {
-            $.ajax({
-                type: 'POST',
-                url: '/api/sessions',
-                data: $(this).serialize(),
-                success: _showOrHideLoginForm
+    LoginLogout = function(app) {
+        var _this = this;
+
+        this.render = function() {
+            $.ajax("/api/sessions/me")
+                .done(function() { 
+                    $('#login').hide(); 
+                    $('#logout').show();
+                    app.dones.show();
+                })
+                .fail(function() { 
+                    $('#login').show(); 
+                    $('#logout').hide();
+                });
+        },
+        this.init = function() {
+            $('#login-form').on('submit', function() {
+                $.ajax({
+                    type: 'POST',
+                    url: '/api/sessions',
+                    data: $(this).serialize(),
+                    success: _this.render
+                });
+                $(this).find('input[type="password"]').val('');
+                return false;
             });
-            $(this).find('input[type="password"]').val('');
-            return false;
-        });
-        $('#logout-link').on('click', function() {
-            $.ajax({
-                type: 'DELETE',
-                url: '/api/sessions/me',
-                success: _showOrHideLoginForm
+            $('#logout-link').on('click', function() {
+                $.ajax({
+                    type: 'DELETE',
+                    url: '/api/sessions/me',
+                    success: _this.render
+                });
+                app.dones.delete();
+                return false;
             });
-            $('#content').html('');
-            return false;
-        });
+        }
     },
     startup = function() {
-        _loginForm();
-        _showOrHideLoginForm();
+        var dones = new Dones();
+        var app = {dones:dones};
+        var login = new LoginLogout(app);
+
+        login.init();
+        login.render();
     };
 
 
